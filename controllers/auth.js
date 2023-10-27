650f3f3a42d5087d9f9cb9b2f10bd30a9c637b35
davidogbaudu <ogbaudu.dc@gmail.com>
2023-10-27 17:02:03 +0100

    added controller for auth route

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,7 +2,19 @@ const mongodb = require("../db/connect")
 const ObjectId = require("mongodb").ObjectId
 const {templeDataSchema, idSchema} = require('../validator/schemas')
 
-const getAll = async (req, res, next) => {
+const SignIn = async (req, res, next) => {
+    const result = await mongodb
+        .getDb()
+        .db()
+        .collection("temples")
+        .find();
+    result.toArray().then((lists) => {
+        res.setHeader("Content-Type", "application/json");
+        res.status(200).json(lists)
+    });
+  };
+
+  const SignInWith = async (req, res, next) => {
     const result = await mongodb
         .getDb()
         .db()
