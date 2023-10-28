b7fed52dd281f36347e712a0a2047f706c3c3554
davidogbaudu <ogbaudu.dc@gmail.com>
2023-10-28 11:06:56 +0100

    ran swagger autogen

--- swagger.js
+++ swagger.js
@@ -12,9 +12,9 @@ const doc = {
 const outputFile = './swagger.json'
 const endpointsFiles = ['./routes/index.js']
 
-// swaggerAutogen(outputFile, endpointsFiles, doc)
+swaggerAutogen(outputFile, endpointsFiles, doc)
 
 
-swaggerAutogen(outputFile, endpointsFiles, doc).then(async () => {
-    await import('./app.js'); // Your project's root file
-  });
+// swaggerAutogen(outputFile, endpointsFiles, doc).then(async () => {
+//     await import('./app.js'); // Your project's root file
+//   });
