e57a2995a32994f1d99597d58b48495a4522a5ab
davidogbaudu <ogbaudu.dc@gmail.com>
2023-11-03 10:39:43 +0100

    Made changes to outh routes and git dev settings

--- controllers/auth.js
+++ controllers/auth.js
@@ -15,21 +15,20 @@ const Oauth = async (req, res, next) => {
     res.redirect(
         `https://github.com/login/oauth/authorize?client_id=${client_id}&client_secret=${client_secret}`
     )
-    res.json()
 };
 
-// const handleCallback = async ( {query : {code}}, res) => {
-//     const body = {
-//         client_id: client_id,
-//         client_secret: client_secret,
-//         code
-//     }
-//     routes.post("https://github/login/oauth/access_token", body, {headers: {accept: 'application/json'}})
-//     .then((_res) => _res.data.access.token)
-//     .then((token) => {
-//         console.log('My token: ', token)
-//         res.redirect(`/?token=${token}`)
-//     })
-// }
+const handleCallback = async ( {query : {code}}, res) => {
+    const body = {
+        client_id: client_id,
+        client_secret: client_secret,
+        code
+    }
+    routes.post(`https://github/login/oauth/access_token?client_id=${client_id}&client_secret=${client_secret}`, body, {headers: {accept: 'application/json'}})
+    .then((_res) => _res.data.access.token)
+    .then((token) => {
+        console.log('My token: ', token)
+        res.redirect(`/?token=${token}`)
+    })
+}
 
-module.exports = {Oauth}
+module.exports = {Oauth, handleCallback}
