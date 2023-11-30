0963fed3b1adac956c378d7e1dd8e486d6e5efcd
davidogbaudu <ogbaudu.dc@gmail.com>
2023-11-30 11:05:13 +0100

    .

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,11 +3,8 @@ dotenv.config();
 const axios = require('axios')
 const router= require('express').Router()
 
-// const client_id = process.env.OUTH_CLIENT_ID
-// const client_secret = process.env.OUTH_CLIENT_SECRET
-
-const client_id = "64461404b9d57c2f9c6bad2ad6b9d3cc7347d8a4"
-const client_secret = "64461404b9d57c2f9c6bad2ad6b9d3cc7347d8a4"
+const client_id = process.env.OUTH_CLIENT_ID
+const client_secret = process.env.OUTH_CLIENT_SECRET
 
 
 const Oauth = async (req, res, next) => {
